47eb30026d170cf64a501912bab51f38209c840b
AlexDeltax <alexsmolenskyi@gmail.com>
2018-06-18 13:07:42 +0300

    OTA link

--- product/ota.mk
+++ product/ota.mk
@@ -1,3 +1,3 @@
 # OTA
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    lineage.updater.uri=https://rolex.genix.space/api/v1/{device}/{type}/{incr}
+    lineage.updater.uri=https://los.genix.space/api/v1/{device}/{type}/{incr}
